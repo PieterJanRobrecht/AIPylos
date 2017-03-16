05add8d84dd12a9750409226f43b080afc5e963a
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2017-03-16 16:39:47 +0100

    init hopelijk maak ik niks kapot voor jouw Matthijs

---
+++ Pylos Java/pylos-core/src/main/java/be/kuleuven/pylos/battle/BattleMain.java
@@ -0,0 +1,15 @@
+package be.kuleuven.pylos.battle;
+
+import be.kuleuven.pylos.player.codes.PylosPlayerBestFit;
+import be.kuleuven.pylos.player.codes.PylosPlayerMiniMax;
+
+/**
+ * Created by Jan on 23/02/2015.
+ */
+public class BattleMain {
+
+	public static void main(String[] args){
+		Battle.play(new PylosPlayerMiniMax(), new PylosPlayerBestFit(), 100);
+	}
+
+}
