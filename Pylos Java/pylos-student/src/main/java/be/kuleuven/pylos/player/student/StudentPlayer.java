055ed328ec8a61d936796915032ebb7a17b1900f
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2017-05-06 12:28:44 +0200

    Begin van AI speler

--- Pylos Java/pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayer.java
+++ Pylos Java/pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayer.java
@@ -2,10 +2,16 @@ package be.kuleuven.pylos.player.student;
 
 import be.kuleuven.pylos.game.*;
 import be.kuleuven.pylos.player.PylosPlayer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Created by Jan on 20/02/2015.
  */
 public class StudentPlayer extends PylosPlayer {
+	PylosLocation lastPylosLocation = null;
 
 	@Override
 	public void doMove(PylosGameIF game, PylosBoard board) {
@@ -25,14 +31,33 @@ public class StudentPlayer extends PylosPlayer {
 	public void doRemove(PylosGameIF game, PylosBoard board) {
 		/* game methods
 			* game.removeSphere(mySphere); */
-
+		PylosSphere sphereToRemove = lastPylosLocation.getSphere();
+		game.removeSphere(sphereToRemove);
 	}
 
 	@Override
 	public void doRemoveOrPass(PylosGameIF game, PylosBoard board) {
-		/* game methods
-			* game.removeSphere(mySphere);
-			* game.pass() */
+		/* collect all removable spheres */
+		ArrayList<PylosSphere> removableSpheres = new ArrayList<>();
+		for (PylosSphere ps : board.getSpheres(this)) {
+			if (ps.canRemove()) {
+				removableSpheres.add(ps);
+			}
+		}
+		Collections.shuffle(removableSpheres, getRandom());
 
+		/* if remove a sphere, remove the sphere with minimum in square
+		 * otherwise, pass */
+		if (!removableSpheres.isEmpty()) {
+			PylosSphere sphere = Collections.min(removableSpheres, new Comparator<PylosSphere>() {
+				@Override
+				public int compare(PylosSphere o1, PylosSphere o2) {
+					return Integer.compare(o1.getLocation().getMaxInSquare(StudentPlayer.this), o2.getLocation().getMaxInSquare(StudentPlayer.this));
+				}
+			});
+			game.removeSphere(sphere);
+		} else {
+			game.pass();
+		}
 	}
 }
